edd46df84071c9ffdb8ddb5866fca2b3a23ae3d2
Lubomir Pandurski <108551504+LubomirPandurski@users.noreply.github.com>
2022-09-19 20:59:35 +0300

    Add files via upload

---
+++ Lists - Exercise/Train.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Train {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> wagons = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        int maxCapacity = Integer.parseInt(scanner.nextLine());
+        String line = scanner.nextLine();
+        while(!line.equals("end")){
+            String [] data = line.split(" ");
+            if(data[0].equals("Add")){
+                wagons.add(Integer.parseInt(data[1]));
+            }else {
+                int passengers = Integer.parseInt(data[0]);
+                for (int i = 0; i < wagons.size(); i++) {
+                    if(wagons.get(i) + passengers<=maxCapacity){
+                        wagons.set(i, wagons.get(i) + passengers);
+                        break;
+                    }
+                }
+            }
+            line = scanner.nextLine();
+        }
+        wagons.forEach(el-> System.out.print(el + " "));
+    }
+}
